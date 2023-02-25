7c1afb63daade4a81c81d338614feeb441952567
ThatBlokeJosh <joshtomrem@protonmail.com>
2023-02-25 22:04:44 +0100

    Added some form of encryption

--- go.go
+++ go.go
@@ -2,37 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"strconv"
+	"time"
 )
 
-func random() int32 {
-	time := time.Now();
+func random(time time.Time) int32 {
 	var secondsStr string = time.Format(".000");
 	seconds, err := strconv.ParseFloat(secondsStr, 32);
 	if err!= nil {
         panic(err);
     }
 
-	randomNum := int32(seconds * 100)
+	randomNum := int32(seconds * 10)
+	if randomNum == 0 {
+		randomNum = 1
+    }
 	return randomNum;
 
 }
 
 func main() {
-	// slice of the alphabet
-	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	randomNum := random();
+	var input string;
+	fmt.Print("Enter a string: ");
+	fmt.Scan(&input);
+	presentTime := time.Now()
+	randomNum := random(presentTime)
 
-	var i int32 = 0;
-	for i < randomNum {
-		if randomNum >= 26 {
-			randomNum -= 26;
-		}
-		i++;
+	for i := 0; i < len(input); i++ {
+		fmt.Printf("%c", input[i] + byte(randomNum));
 	}
-
-    fmt.Println(alphabet[randomNum])
-
-
+	println()
 }
